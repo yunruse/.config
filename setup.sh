ac28a4ad9250548ffbf94f5406988406fc903699
Mia yun Ruse <mia@yunru.se>
2023-08-25 09:21:05 +0100

    misc changes
    i use `micro` and `vscode` seemingly interchangeably so this is convenient lol

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ ln -fs "~/Library/Mobile Documents/com~apple~CloudDocs" ~/iCloud
 ln -fs ~/iCloud/Projects ~/Projects
 
 # .config symlinks
+
+# TODO: invert these for portability - .config is your source of truth!
 ln -fs ~/.config/zsh/.zlogin ~/.zlogin
 ln -fs ~/.config/zsh/.zshrc ~/.zshrc
 ln -fs ~/.config/ssh ~/.ssh
