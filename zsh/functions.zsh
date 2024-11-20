8b5a2e03c84769d4429660effdde15658da3b877
Mia yun Ruse <mia@yunru.se>
2024-11-20 12:51:35 +0000

    Update functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,13 +2,20 @@
 ## Functions and aliases
 
 
-
-# Editor
+# Editor aliases
 export EDITOR=micro
 export VISUAL=$EDITOR
 alias edit="$EDITOR"
 alias nano="micro"
 
+
+function zedit {
+  nano ~/.config/zsh/functions.zsh
+  source ~/.config/zsh/functions.zsh
+}
+alias zclear="zsh ~/.zlogin"
+
+source ~/Projects/transmission-control/aliases.sh
 
 # Filesystem
 alias ls="eza -hbF --git --no-user -s type"
@@ -18,6 +25,11 @@ function mkcd { mkdir $1 && cd $1 }
 function mvself {
 	mv "$(pwd)" $*
 	cd .
+}
+
+function transmog {
+	ffmpeg -i ~/Downloads/$1.mov ~/Downloads/$1.mp4
+	rm ~/Downloads/$1.mov
 }
 
 
@@ -37,39 +49,3 @@ function import { python -ic "import $*" }
 alias noether="python -im noether"
 
 
-# Recursion!
-function zedit {
-  nano ~/.config/zsh/functions.zsh
-  source ~/.config/zsh/functions.zsh
-}
-
-
-# Dashboard
-alias zclear="zsh ~/.zlogin"
-
-alias dash="zclear -n; noe; cd"
-function noe {
-  # Development dashboard for Noether
-  pushd ~/Projects/Noether >/dev/null
-  make type --silent
-  make test --silent 
-  python3 ~/.config/zsh/noether_units.py
-}
-
-# Menubar
-function start-countdown-menubar {
-  countdown-menubar ~/.config/zsh/calendar.txt -aC 'Book progress' &
-}
-
-
-# Misc
-function duration-dir {
-  # List total duration of all videos (recursive) in this directory
-  # https://news.ycombinator.com/item?id=37384212
-  find . -type f -print0 | \
-      xargs -0 -P 10 -I {} ffprobe -v quiet -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {} | \
-      awk '{s += $1} END {printf "%dh%.2dm%.2ds\n", s/3600, s%3600/60, s%3600%60}'
-}
-
-alias valeveno="~/Projects/duskbreak/languages/translate"
-source ~/Projects/transmission-control/aliases.sh
